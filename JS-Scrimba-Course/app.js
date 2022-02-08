832980668b3badb9aab50601b751e6975c280970
Wesley Endliche <wesleyhoo@hotmail.com>
2022-02-08 11:02:41 -0300

    first steps into the course

---
+++ JS-Scrimba-Course/app.js
@@ -0,0 +1,15 @@
+/*document.getElementById("contador").innerText = 3;
+
+let firstBatch = 5;
+let secondBatch = 7;
+
+let count = firstBatch + secondBatch;
+
+console.log(count);*/
+
+let myAge = 29;
+let humanDogRatio = 7;
+
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge)
