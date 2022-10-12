d65f46b5230cb61ace04285d006d32e4a9c0a85f
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2022-10-12 19:09:51 -0300

    added app js

---
+++ 100-days-of-code/app.js
@@ -0,0 +1,12 @@
+let age = 30;
+let userName = 'Wesley';
+let hobbies = ['Reading', 'Video games', 'Music'];
+let job = { 
+    title: 'Developer',
+    place: 'Toronto',
+    salary: 50000,
+};
+
+age = 45;
+let adultYears = age - 18;
+console.log(adultYears);
