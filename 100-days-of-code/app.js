949bf8110a0edd4d348ff383e1975e8d72f7e827
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2022-10-16 10:30:56 -0300

    added new exercise

--- 100-days-of-code/app.js
+++ 100-days-of-code/app.js
@@ -1,1 +1,7 @@
-document.body.children[1].children[0].href = 'https://google.com';
+// document.body.children[1].children[0].href = 'https://google.com';
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = "https://google.com";
+
+anchorElement = document.querySelector('p a');
+anchorElement.href = 'https://academind.com';
