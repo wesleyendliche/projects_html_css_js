3bb13f53ef5291de74f426c861760852e40e208f
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2022-10-17 09:12:35 -0300

    updates

--- 100-days-of-code/app.js
+++ 100-days-of-code/app.js
@@ -8,3 +8,14 @@ function changeParagraphText() {
 paragraphElement.addEventListener('click', changeParagraphText);
 //we don't add () to the function name here because we don't want to call the action automatically
 //Instead, we wait for the user to call the action.
+
+let inputElement = document.querySelector('input');
+
+function retrieveUserInput(event) {
+    //let enteredText = inputElement.value;
+    let enteredText = event.target.value;
+    console.log(enteredText);
+    //console.log(event);
+}
+
+inputElement.addEventListener('input', retrieveUserInput);
