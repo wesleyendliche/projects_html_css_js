038dc15ed663b78e0347482266cd172a7996ddc2
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2022-11-11 10:59:11 -0300

    project done

--- 53-Tic-tac-toe/scripts/config.js
+++ 53-Tic-tac-toe/scripts/config.js
@@ -1,38 +1,32 @@
-function openPlayerConfig() {
-    editedPlayer = +event.target.dataset.playerid; //related to 'data' attribute in HTML; + to covert string to a number
-    playerConfigOverlay.style.display = 'block';
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid; // +'1' => 1
+    playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
-}
-
-function closePlayerConfig() {
-    playerConfigOverlay.style.display = 'none';
+  }
+  
+  function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
     formElement.firstElementChild.classList.remove('error');
     errorsOutputElement.textContent = '';
     formElement.firstElementChild.lastElementChild.value = '';
-}
-
-function savePlayerConfig(event) {
+  }
+  
+  function savePlayerConfig(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const enteredPlayerName = formData.get('playername').trim(); //'    Wes  ' => 'Wes'
-
-    if (!enteredPlayerName) { // enteredPlayerName === ''
-        event.target.firstElementChild.classList.add('error');
-        errorsOutputElement.textContent = 'Please enter a valid name!';
-        return; // not execute other code when return is activated
+    const enteredPlayername = formData.get('playername').trim(); // '      ' => ''
+  
+    if (!enteredPlayername) { // enteredPlayername === ''
+      event.target.firstElementChild.classList.add('error');
+      errorsOutputElement.textContent = 'Please enter a valid name!';
+      return;
     }
-
+  
     const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
-    updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
-
-    // if (editedPlayer === 1) {
-    //     players[0].name = enteredPlayerName;
-    // } else {
-    //     players[1].name = enteredPlayerName;
-    // }
-
-    players[editedPlayer - 1].name = enteredPlayerName;
-
-    closePlayerConfig();
-}
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+  
+    players[editedPlayer - 1].name = enteredPlayername;
+  
+    closePlayerConfig();  
+  }
--- 53-Tic-tac-toe/scripts/game.js
+++ 53-Tic-tac-toe/scripts/game.js
@@ -1,12 +1,33 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML =
+    'You won, <span id="winner-name">PLAYER NAME</span>!';
+  gameOverElement.style.display = 'none';
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = '';
+      gameBoardItemElement.classList.remove('disabled');
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
-  if (players[0].name === "" || players[1].name === "") {
-    alert("Please set custom player names for both players!");
+  if (players[0].name === '' || players[1].name === '') {
+    alert('Please set custom player names for both players!');
     return;
   }
 
-  activePlayerNameElement.textContent = players[activePlayer].name;
+  resetGameStatus();
 
-  gameAreaElement.style.display = "block";
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = 'block';
 }
 
 function switchPlayer() {
@@ -15,12 +36,11 @@ function switchPlayer() {
   } else {
     activePlayer = 0;
   }
-
   activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
 function selectGameField(event) {
-  if (event.target.tagName !== "LI") {
+  if (event.target.tagName !== 'LI' || gameIsOver) {
     return;
   }
 
@@ -29,25 +49,28 @@ function selectGameField(event) {
   const selectedRow = selectedField.dataset.row - 1;
 
   if (gameData[selectedRow][selectedColumn] > 0) {
-    alert("Please select an empty field");
+    alert('Please select an empty field!');
     return;
   }
 
   selectedField.textContent = players[activePlayer].symbol;
-  selectedField.classList.add("disabled");
+  selectedField.classList.add('disabled');
 
-  gameData[selectedRow][selectedColumn] = activePlayer + 1; //[select the array] [item in this array]
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
+
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
 
   currentRound++;
   switchPlayer();
 }
 
 function checkForGameOver() {
-  //Checking the rows for equality
-  for (let i = 0; i < 3; i++)
+  // Checking the rows for equality
+  for (let i = 0; i < 3; i++) {
     if (
       gameData[i][0] > 0 &&
       gameData[i][0] === gameData[i][1] &&
@@ -55,9 +78,10 @@ function checkForGameOver() {
     ) {
       return gameData[i][0];
     }
+  }
 
-  //Checking the columns for equality
-  for (let i = 0; i < 3; i++)
+  // Checking the columns for equality
+  for (let i = 0; i < 3; i++) {
     if (
       gameData[0][i] > 0 &&
       gameData[0][i] === gameData[1][i] &&
@@ -65,8 +89,9 @@ function checkForGameOver() {
     ) {
       return gameData[0][i];
     }
+  }
 
-  //Diagonal: Top left to bottom right
+  // Diagonal: Top left to bottom right
   if (
     gameData[0][0] > 0 &&
     gameData[0][0] === gameData[1][1] &&
@@ -75,7 +100,7 @@ function checkForGameOver() {
     return gameData[0][0];
   }
 
-  //Diagonal: Bottom left to top right
+  // Diagonal: Bottom left to top right
   if (
     gameData[2][0] > 0 &&
     gameData[2][0] === gameData[1][1] &&
@@ -89,4 +114,17 @@ function checkForGameOver() {
   }
 
   return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = 'block';
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw!";
+  }
 }
