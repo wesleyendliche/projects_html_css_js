f2bb1d4079d0afb9b5c9e728840ec59865d8c04b
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2023-01-05 07:47:08 -0300

    Update main.js

--- alura_midi/scripts/main.js
+++ alura_midi/scripts/main.js
@@ -1,21 +1,36 @@
-function tocaSom(idAudio) {
-  document.querySelector(idAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  } else {
+    console.log('Elemento não encontrado ou seletor inválido');
+  }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < listaDeTeclas.length) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
-
-  //template string
-  const idAudio = `#som_${instrumento}`;
+  const idAudio = `#som_${instrumento}`; //template string
 
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-  
-  contador++;
+
+  tecla.onkeydown = function (evento) {
+
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+    
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+
 }
