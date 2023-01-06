83215704cfc4001addd6ccc86410f79100f8b143
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2023-01-06 14:09:18 -0300

    started js file

---
+++ alura_dom_robotron/js/main.js
@@ -0,0 +1,10 @@
+const robotron = document.querySelector("#robotron");
+
+robotron.addEventListener("click", dizOi);
+
+function dizOi() {
+  console.log("oi");
+  console.log("Bem-vindo ao Robotron 2000");
+}
+
+dizOi();
