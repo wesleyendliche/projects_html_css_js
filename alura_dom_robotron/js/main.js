4262aec1b49a5635ba354b50f82ca468d3c5072d
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2023-01-07 10:19:55 -0300

    more updates on js

--- alura_dom_robotron/js/main.js
+++ alura_dom_robotron/js/main.js
@@ -1,19 +1,17 @@
-const subtrair = document.querySelector('#subtrair');
-const adicionar = document.querySelector('#adicionar');
-const braco =  document.querySelector('#braco');
-
 const controle = document.querySelectorAll('.controle-ajuste');
 
 controle.forEach( (elemento) => {
-  elemento.addEventListener('click', () => {
-    manipulaDados(event.target.textContent);
+  elemento.addEventListener('click', (evento) => {
+    manipulaDados(event.target.textContent, evento.target.parentNode);
   })
 })
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+  const peca =  controle.querySelector('.controle-contador');
+
   if (operacao === "-") {
-    braco.value = parseInt(braco.value) - 1;
+    peca.value = parseInt(peca.value) - 1;
   } else {
-    braco.value = parseInt(braco.value) + 1;
+    peca.value = parseInt(peca.value) + 1;
   }
 }
