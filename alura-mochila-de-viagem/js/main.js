c6e86476bc339e672739388a35814730747b34d6
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2023-01-11 14:22:56 -0300

    conclusion

--- alura-mochila-de-viagem/js/main.js
+++ alura-mochila-de-viagem/js/main.js
@@ -12,25 +12,21 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-
-//  Const para conferir elemento nome no array itens 
-    const existe = itens.find( elemento => elemento.nome === nome.value ) 
+    const existe = itens.find( elemento => elemento.nome === nome.value )
 
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-
-// Condicional para conferir se o elemento 
     if (existe) {
         itemAtual.id = existe.id
-
+        
         atualizaElemento(itemAtual)
 
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-       itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
 
         criaElemento(itemAtual)
 
@@ -51,12 +47,33 @@ function criaElemento(item) {
     numeroItem.innerHTML = item.quantidade
     numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
-
+    
     novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta(item.id))
 
     lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
