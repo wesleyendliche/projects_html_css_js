b20d66ddbdffed54439f675f6e4810a677a65408
Wesley Endliche <90987648+wesleyendliche@users.noreply.github.com>
2023-04-12 11:01:38 -0300

    Add logic and more styles

---
+++ alura-numero-secreto/app/reconhecimentoDeVoz.js
@@ -0,0 +1,50 @@
+const elementoChute = document.getElementById('chute')
+
+const numeros = {
+    'zero zero': 0,
+    '00': 0,
+    '01': 1,
+    'um': 1,
+    'dois': 2,
+    'três': 3,
+    'quatro': 4,
+    'cinco': 5,
+    'seis': 6,
+    'sete': 7,
+    'oito': 8,
+    'nove': 9,
+    'dez': 10
+}
+ 
+const corrigeNumeros = (palavra) => {
+    for(numero in numeros){
+       if(palavra === numero){
+          palavra = numeros[numero];   
+       }         
+    }
+    return palavra;
+}
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaValor(chute)
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+    <div>Você disse</div>
+    <span class="box">${chute}</span>
+    `
+}
+
+recognition.addEventListener('end', () => recognition.start())
---
+++ alura-numero-secreto/app/validacao.js
@@ -0,0 +1,36 @@
+function verificaValor(chute) {
+    const numero = +chute
+
+    if(chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if(numeroForMaiorOuMenorQueOPermitido(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    } else if(numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        ` 
+    }
+}
+
+function numeroForMaiorOuMenorQueOPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
